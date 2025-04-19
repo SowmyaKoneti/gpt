c4f3dae04964b87df355f925d0c21b03baa32f2b
SowmyaKoneti <sowmyaa.koneti@gmail.com>
2025-04-19 12:59:06 -0500

    changed a/Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
+'use client';
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-black-50 text-center">
       <h1 className="text-3xl font-bold mb-4">Please login to continue</h1>
-      <a
+      <Link
         href="/login"
         className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
       >
         Login
-      </a>
+      </Link>
     </main>
   );
 }
