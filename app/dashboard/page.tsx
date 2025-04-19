26402c872804c8a37f06ed064c7704cb4d7f1c03
SowmyaKoneti <sowmyaa.koneti@gmail.com>
2025-04-19 12:44:22 -0500

    added login link in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,16 +4,16 @@ import { useUser } from '@clerk/nextjs';
 import { useClerk } from '@clerk/nextjs'
 
 export default function DashboardPage() {
-    const { isSignedIn, user, isLoaded } = useUser();
+
     const { signOut } = useClerk()
 
-    if (!isLoaded) return <div>Loading...</div>;
-    if (!isSignedIn) return null;
+    const { user } = useUser();
+
 
     return (
         <main className="p-6">
             <h1 className="text-2xl font-bold mb-4">
-                Welcome!!, {user.firstName || user.username}
+                Welcome!!, {user?.firstName} {user?.lastName}
             </h1>
             <button onClick={() => signOut({ redirectUrl: '/login' })}>
                 Signout
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>WELCOME</h1>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black-50 text-center">
+      <h1 className="text-3xl font-bold mb-4">Please login to continue</h1>
+      <a
+        href="/login"
+        className="px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+      >
+        Login
+      </a>
+    </main>
   );
 }
